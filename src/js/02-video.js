1a4233c4af3815627b7ea14ee0d8703effb3a11f
TyllerDurden97 <albatros97dn@gmail.com>
2023-03-13 17:08:15 +0200

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,19 +4,14 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-function savePlayedTime(event) {
+   function savePlayedTime(event) {
      localStorage.setItem("videoplayer-current-time", event.seconds);
   }
 
     player.on('timeupdate', throttle(savePlayedTime, 1000));
 
 const savedPlayedTime = localStorage.getItem("videoplayer-current-time");
-        console.log(savedPlayedTime);
+      //   console.log(savedPlayedTime);
 
 player.setCurrentTime(savedPlayedTime);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.getElementsByName("email");
+const button = form.lastElementChild;
+const textArea = document.getElementsByName("message");
+console.dir(emailInput);
+console.log(emailInput.value);
+
+
+
+emailInput.addEventListener('input', storeFormData);
+
+function storeFormData(event) {
+
+   // const storedData = {};
+   // storedData.email = emailInput.currentTarget.value;
+
+console.dir(event);
+
+}
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+      event.preventDefault();
+
+}
